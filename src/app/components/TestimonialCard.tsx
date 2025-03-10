184a54b197ad28799dfd1191b317b8ad59920874
Aubrey Barnes <aubrey.hbarnes@gmail.com>
2025-03-10 11:35:37 -0500

    escape apostrophe's?

--- src/app/components/TestimonialCard.tsx
+++ src/app/components/TestimonialCard.tsx
@@ -18,8 +18,8 @@ export default function TestimonialCard() {
                 </div>
             </div>
             <div className="copy-container text-gray-600 pt-6">
-                <p>I've been searching for high-quality abstract images for my design projects, and 
-                    I'm thrilled to have found this platform. The variety and depth of creativity
+                <p>I&apos;ve been searching for high-quality abstract images for my design projects, and 
+                    I&apos;m thrilled to have found this platform. The variety and depth of creativity
                     are astounding!
                 </p>
             </div>
